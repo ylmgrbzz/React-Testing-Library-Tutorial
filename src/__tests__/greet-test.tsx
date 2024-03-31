7f1c06e97a74922f2a76174445e66e96262a668f
ylmgrbzz <yalim.gurbuz@std.yildiz.edu.tr>
2024-03-31 22:55:48 +0300

    Update function to handle edge cases

---
+++ src/__tests__/greet-test.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+import { render, screen } from "@testing-library/react";
+import Greet, { GreetTest } from "../components/greet/greet";
+
+describe("greet", () => {
+  test("greet", () => {
+    render(<Greet />);
+    const linkElement = screen.getByText(/hello/i);
+    const linkElement2 = screen.getByText("hello");
+    expect(linkElement).toBeInTheDocument();
+    expect(linkElement2).toBeInTheDocument();
+
+    expect(true).toBe(true);
+  });
+
+  describe("greetNested", () => {
+    test.skip("greet2", () => {
+      render(<GreetTest name="John" />);
+      const linkElement = screen.getByText("Hello John");
+      expect(linkElement).toBeInTheDocument();
+
+      expect(true).toBe(true);
+    });
+  });
+});
