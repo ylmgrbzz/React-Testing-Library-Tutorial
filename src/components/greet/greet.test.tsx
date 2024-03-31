d24ed2a2ef6ed47ed55864bef4026f385207f55d
ylmgrbzz <yalim.gurbuz@std.yildiz.edu.tr>
2024-03-31 22:43:04 +0300

    Skip tests for greet component

--- src/components/greet/greet.test.tsx
+++ src/components/greet/greet.test.tsx
@@ -2,20 +2,22 @@ import React from "react";
 import { render, screen } from "@testing-library/react";
 import Greet, { GreetTest } from "./greet";
 
-test("greet", () => {
-  render(<Greet />);
-  const linkElement = screen.getByText(/hello/i);
-  const linkElement2 = screen.getByText("hello");
-  expect(linkElement).toBeInTheDocument();
-  expect(linkElement2).toBeInTheDocument();
+describe.skip("greet", () => {
+  test("greet", () => {
+    render(<Greet />);
+    const linkElement = screen.getByText(/hello/i);
+    const linkElement2 = screen.getByText("hello");
+    expect(linkElement).toBeInTheDocument();
+    expect(linkElement2).toBeInTheDocument();
 
-  expect(true).toBe(true);
-});
+    expect(true).toBe(true);
+  });
 
-test("greet2", () => {
-  render(<GreetTest name="John" />);
-  const linkElement = screen.getByText("Hello John");
-  expect(linkElement).toBeInTheDocument();
+  test.skip("greet2", () => {
+    render(<GreetTest name="John" />);
+    const linkElement = screen.getByText("Hello John");
+    expect(linkElement).toBeInTheDocument();
 
-  expect(true).toBe(true);
+    expect(true).toBe(true);
+  });
 });
