44d8ff0d73e70c36adbd5eeba61375a596ddcdd1
ylmgrbzz <yalim.gurbuz@std.yildiz.edu.tr>
2024-04-05 10:22:35 +0300

    Add AppProviders component to App.js

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,13 @@ import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
 import { Application } from "./components/application/application";
+import { AppProviders } from "./providers/app-providers";
 
 function App() {
   return (
-    <div className="App">
-      <Application />
-    </div>
+    <AppProviders>
+      <div className="App">{/* <Application /> */}</div>
+    </AppProviders>
   );
 }
 
